3bc2e3b3f2ef608810b24f66254170fe6e803718
sarim2000 <sarimbleedblue@gmail.com>
2023-01-20 01:24:01 +0530

    upd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
